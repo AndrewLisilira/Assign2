b21ec18be8a6481c15ec36cc8eee6c55812c4626
AndrewLisilira <73199339+AndrewLisilira@users.noreply.github.com>
2020-11-09 13:23:53 -0800

    Add files via upload

---
+++ StudentAccount.java
@@ -0,0 +1,29 @@
+package Andy;
+
+public class StudentAccount extends Account{
+
+	// data fields 
+	private String regNumber;
+	private boolean isStudentAccount = true;
+	private boolean isExecutiveAccount = false;
+
+	// constructors
+	StudentAccount(){
+	  Account.setNumberOfAccounts();
+	  setAccountId();
+	}
+
+	// methods
+	public void setRegNumber(String regNumber){
+	  this.regNumber = regNumber;
+	}
+	public String getRegNumber(){
+	  return regNumber;
+	}
+	public boolean getIsExecutiveAccount(){
+	  return isExecutiveAccount; 
+	}
+	public boolean getIsStudentAccount(){
+	  return isStudentAccount;
+	}
+	}
